9ee61894dfdb752db692bc0998244231245761a7
Vincent de Lachaux <vincent.delachaux@4d.com>
2023-07-11 14:44:54 +0200

    19R8

---
+++ 4DPop-Family-19R8/Components/4DPop QuickOpen.4dbase/Documentation/Methods/quickOpenInit.md
@@ -0,0 +1,12 @@
+<!-- quickOpenInit(Formula(MODIFIERS); Formula(KEYCODE) {; modifierBit {; keyCode }}) -->
+## Description
+
+`quickOpenInit` must be called once, generally in the `On Startup` method to initialize the QuickOpen component and eventually set the main shortcut.
+
+## Syntax
+
+`quickOpenInit()`
+
+## Usage
+
+See the [quickOpenEventHandler method documentation](quickOpenEventHandler.md) or the [complete documentation](https://github.com/vdelachaux/4DPop-QuickOpen/blob/master/README.md) 
---
+++ 4DPop-Family-19R8/Components/4DPop Rulers.4dbase/Resources/Common/lang.js
@@ -0,0 +1,27 @@
+﻿<!--
+
+// Display the localized help file according to the browser lang
+// Uncomment or add a "else if" clause
+
+if (navigator.browserLanguage)
+	var language = navigator.browserLanguage;
+else
+	var language = navigator.language;
+		
+if (language.indexOf('fr') > -1) document.location.href = 'Resources/French.lproj/Help/Help.html';
+		
+	else if (language.indexOf('en') > -1) document.location.href = 'Resources/English.lproj/Help/Help.html';
+		
+	else if (language.indexOf('ja') > -1) document.location.href = 'Resources/Japanese.lproj/Help/Help.html';
+		
+	else if (language.indexOf('es') > -1) document.location.href = 'Resources/Spanish.lproj/Help/Help.html';
+		
+	else if (language.indexOf('de') > -1) document.location.href = 'Resources/German.lproj/Help/Help.html';
+		
+//	else if (language.indexOf('it') > -1) document.location.href = 'Resources/Italian.lproj/Help/Help.html';
+		
+else
+		
+	document.location.href = 'Resources/English.lproj/Help/Help.html';
+
+// -->		
